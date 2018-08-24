63bfd84d369e2c79fb93cb498b3619acd0dc4272
Germán Valero <gvalero@unam.mx>
2018-08-24 10:03:18 -0500

    suggested changes to English language strings
    These minor changes improve the English language strings

--- csv/lang/en/qformat_csv.php
+++ csv/lang/en/qformat_csv.php
@@ -23,8 +23,8 @@
  */
 
 $string['pluginname'] = 'CSV format';
-$string['pluginname_help'] = 'This is a CSV format for importing multiple choice( 2 choices ) or single choice questions from a csv file.  <a href="'.$CFG->wwwroot.'/question/format/csv/sample.php" >Click here </a>to download sample file.';
+$string['pluginname_help'] = 'This is a CSV format for importing multiple choice ( 2 choices ) or single choice questions from a csv file.  <a href="'.$CFG->wwwroot.'/question/format/csv/sample.php" >Click here </a>to download sample file.';
 $string['pluginname_link'] = 'qformat/csv';
-$string['commma_error'] = '<font color="#990000"> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field. <br /></font>';
-$string['newline_error'] = '<font color="#990000">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been improted.</font>';
+$string['commma_error'] = '<font color="#990000"> Upload failed. Unnecessary Comma (,) found in <b> Question {$a} </b><br /> Please remove the comma (,) from the field. <br /></font>';
+$string['newline_error'] = '<font color="#990000">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been imported.</font>';
 $string['samplefile'] = 'SampleFile';
