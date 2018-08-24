06562e37aa3a24536d37c70327798204ef113271
Gopal Sharma <gopal@ilearn.trigyn.com>
2018-08-24 19:39:05 +0530

    fixed #5 issue related to Language file containing calculated properties.

--- csv/lang/en/qformat_csv.php
+++ csv/lang/en/qformat_csv.php
@@ -23,8 +23,9 @@
  */
 
 $string['pluginname'] = 'CSV format';
-$string['pluginname_help'] = 'This is a CSV format for importing multiple choice( 2 choices ) or single choice questions from a csv file.  <a href="'.$CFG->wwwroot.'/question/format/csv/sample.php" >Click here </a>to download sample file.';
+$string['pluginname_help'] = 'This is a CSV format for importing multiple choice( 4 choices ) questions with one or maximum of two answers from a  CSV(Comma separated value) file. Please find the sample csv(sample.csv) file sent along with this plugin for your reference.';
 $string['pluginname_link'] = 'qformat/csv';
 $string['commma_error'] = '<font color="#990000"> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field. <br /></font>';
 $string['newline_error'] = '<font color="#990000">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been improted.</font>';
 $string['samplefile'] = 'SampleFile';
+$string['privacy:metadata'] = 'The CSV format plugin does not store any personal data.';
