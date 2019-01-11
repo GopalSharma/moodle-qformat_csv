c7a85034432437e767b0c171d8877c49e0368beb
Gopal Sharma <gopalsharma66@gmail.com>
2019-01-11 17:08:09 +0530

    made change in the language string for error to be shown to users.

--- lang/en/qformat_csv.php
+++ lang/en/qformat_csv.php
@@ -23,10 +23,10 @@
  */
 
 $string['pluginname'] = 'CSV format';
-$string['pluginname_help'] = 'This is a CSV format for importing and exporting multiple choice( 4 choices ) questions with one or maximum of two answers from a CSV(Comma separated value) file. Please find the sample csv(sample.csv) file sent along with this plugin for your reference.';
+$string['pluginname_help'] = 'This is a CSV format for importing and exporting multiple choice( 4 choices ) questions with one or maximum of two answers from a CSV(Comma separated value) file. Please find the Sample csv(sample.csv) and Extended Sample csv(extended_sample.csv) file sent along with this plugin for your reference.';
 $string['pluginname_link'] = 'qformat/csv';
-$string['commma_error'] = '<font color="#990000"> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field or Put the entire text between the double quotes(" "), So the coomma(,) between theme can be ignored. <br /></font>';
+$string['commma_error'] = '<font color="#990000"> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field or Put the entire text between the double quotes(" "), So the coomma(,) between them can be ignored. <br /></font>';
 $string['newline_error'] = '<font color="#990000">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been imported.</font>';
-$string['csv_file_error'] = '<font color="#990000">Upload failed. Something went wrong at line number <b/> {$a} . </b> Make sure you are uploading a valid CSV file.<br /> Please correct the error and try importing again. <br /> No Question has been imported.</font>';
+$string['csv_file_error'] = '<font color="#990000">Upload failed. Something went wrong at line number <b/> {$a} . </b> Make sure you are uploading a valid CSV file.<br /> Please check that, <b> Simple CSV file must have 8 headers/fields and Extended CSV file must have 13 headers/fields in order work.</b><br /> Please correct the error and try importing again. <br /> No Question has been imported.</font>';
 $string['samplefile'] = 'SampleFile';
 $string['privacy:metadata'] = 'The CSV format plugin does not store any personal data.';
